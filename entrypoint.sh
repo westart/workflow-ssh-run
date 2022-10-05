c41db5ded7cf011b674b2b8f1265371cfc948d09
IlSerry <marco.serritella@alias2k.com>
2022-10-05 07:49:42 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ if [[ "$INPUT_KEY" ]]; then
     chmod 600 tmp_id
     ssh -P $INPUT_PORT -o StrictHostKeyChecking=no -i tmp_id "$INPUT_USER"@"$INPUT_HOST" 'echo $INPUT_COMMANDS > /tmp/test'
     #scp -qr -P $INPUT_PORT -o StrictHostKeyChecking=no -i tmp_id $INPUT_SOURCE "$INPUT_USER"@"$INPUT_HOST":"$INPUT_DESTINATION"
-else
+#else
     #echo "##### Utilizzo l'autenticazione con password"
     #sshpass -p $INPUT_PASSWORD scp -qr -P $INPUT_PORT -o StrictHostKeyChecking=no $INPUT_SOURCE "$INPUT_USER"@"$INPUT_HOST":"$INPUT_DESTINATION"
 fi
