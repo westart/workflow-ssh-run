2de09844dc8cb4f37062d6f318cb60ebb6da8464
IlSerry <marco.serritella@alias2k.com>
2022-10-05 07:46:19 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
+echo "$INPUT_COMMANDS"
 if [[ "$INPUT_KEY" ]]; then
     echo -e "${INPUT_KEY}" > tmp_id
     chmod 600 tmp_id
-    scp -qr -P $INPUT_PORT -o StrictHostKeyChecking=no -i tmp_id $INPUT_SOURCE "$INPUT_USER"@"$INPUT_HOST":"$INPUT_DESTINATION"
+    ssh -P $INPUT_PORT -o StrictHostKeyChecking=no -i tmp_id "$INPUT_USER"@"$INPUT_HOST" 'echo $INPUT_COMMANDS > /tmp/test'
+    #scp -qr -P $INPUT_PORT -o StrictHostKeyChecking=no -i tmp_id $INPUT_SOURCE "$INPUT_USER"@"$INPUT_HOST":"$INPUT_DESTINATION"
 else
-    echo "##### Utilizzo l'autenticazione con password"
-    sshpass -p $INPUT_PASSWORD scp -qr -P $INPUT_PORT -o StrictHostKeyChecking=no $INPUT_SOURCE "$INPUT_USER"@"$INPUT_HOST":"$INPUT_DESTINATION"
+    #echo "##### Utilizzo l'autenticazione con password"
+    #sshpass -p $INPUT_PASSWORD scp -qr -P $INPUT_PORT -o StrictHostKeyChecking=no $INPUT_SOURCE "$INPUT_USER"@"$INPUT_HOST":"$INPUT_DESTINATION"
 fi
-ERR=$?
-if [[ $? -ne 0 ]]; then
-    echo "Errore nel trasferimento"
-    exit $ERR
-fi
+#ERR=$?
+#if [[ $? -ne 0 ]]; then
+#    echo "Errore nel trasferimento"
+#    exit $ERR
+#fi
