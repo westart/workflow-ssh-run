a05407ab24107f63d0e8e95e2d9f383e9a48ec17
IlSerry <marco.serritella@alias2k.com>
2022-11-04 07:47:41 +0100

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-IFS=$'\n' read -a ARR < "${INPUT_COMMANDS}"
-#echo "${ARR}"
-for i in  "${ARR}"; do echo "eseguo : $i"; done
+echo -e "${INPUT_KEY}" > tmp_id
+chmod 600 tmp_id
+
+while read -r line; do 
+    ssh -P $INPUT_PORT -o StrictHostKeyChecking=no -i tmp_id "$INPUT_USER"@"$INPUT_HOST" $line
+done <<< $INPUT_COMMANDS
+
+#for i in  "${ARR}"; do echo "eseguo : $i"; done
 #if [[ "$INPUT_KEY" ]]; then
-#    echo -e "${INPUT_KEY}" > tmp_id
-#    chmod 600 tmp_id
-    #ssh -P $INPUT_PORT -o StrictHostKeyChecking=no -i tmp_id "$INPUT_USER"@"$INPUT_HOST" 'echo $INPUT_COMMANDS > /tmp/test'
     #scp -qr -P $INPUT_PORT -o StrictHostKeyChecking=no -i tmp_id $INPUT_SOURCE "$INPUT_USER"@"$INPUT_HOST":"$INPUT_DESTINATION"
 #else
     #echo "##### Utilizzo l'autenticazione con password"
