d7b99335757f0128b9a5782d1c6f11ed5511d6ee
IlSerry <marco.serritella@alias2k.com>
2022-11-04 07:20:27 +0100

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-IFS=$'\n' read -a ARR < INPUT_COMMANDS
-for i in  "$INPUT_COMMANDS"; do echo "---$i---"; done
+IFS=$'\n' read -a ARR < $INPUT_COMMANDS
+for i in  "$ARR"; do echo "---$i---"; done
 if [[ "$INPUT_KEY" ]]; then
     echo -e "${INPUT_KEY}" > tmp_id
     chmod 600 tmp_id
