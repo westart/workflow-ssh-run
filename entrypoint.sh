5a810e408e4bc8b6932a0a72f70551c71ede6a59
IlSerry <marco.serritella@alias2k.com>
2022-11-04 07:24:14 +0100

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 IFS=$'\n' read -a ARR < $INPUT_COMMANDS
-for i in  "$ARR"; do echo "---$i---"; done
-if [[ "$INPUT_KEY" ]]; then
-    echo -e "${INPUT_KEY}" > tmp_id
-    chmod 600 tmp_id
+echo "${INPUT_COMMANDS}"
+echo "${ARR}"
+#for i in  "$ARR"; do echo "---$i---"; done
+#if [[ "$INPUT_KEY" ]]; then
+#    echo -e "${INPUT_KEY}" > tmp_id
+#    chmod 600 tmp_id
     #ssh -P $INPUT_PORT -o StrictHostKeyChecking=no -i tmp_id "$INPUT_USER"@"$INPUT_HOST" 'echo $INPUT_COMMANDS > /tmp/test'
     #scp -qr -P $INPUT_PORT -o StrictHostKeyChecking=no -i tmp_id $INPUT_SOURCE "$INPUT_USER"@"$INPUT_HOST":"$INPUT_DESTINATION"
 #else
     #echo "##### Utilizzo l'autenticazione con password"
     #sshpass -p $INPUT_PASSWORD scp -qr -P $INPUT_PORT -o StrictHostKeyChecking=no $INPUT_SOURCE "$INPUT_USER"@"$INPUT_HOST":"$INPUT_DESTINATION"
-fi
+#fi
 #ERR=$?
 #if [[ $? -ne 0 ]]; then
 #    echo "Errore nel trasferimento"
